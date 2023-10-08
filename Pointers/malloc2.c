6c6a03f04196e157e4bb3452ea794faa3f545938
Muyaz Rahman <Muyaz154@gmail.com>
2023-10-08 16:13:12 +0100

    malloc2

---
+++ Pointers/malloc2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Enter the dimensions of a 2d array (m x n).\n");
+        return 1;
+    }
+
+    int m = atoi(argv[1]), n = atoi(argv[2]);
+    int **arr = (int **)malloc(m * sizeof(int *));  
+    
+    for (int i = 0; i < m; i++) {
+        arr[i] = (int *)malloc(n * sizeof(int));
+    }
+    for (int r = 0; r < m; r++) {
+        if (r > 0) printf("\n");
+        for (int c = 0; c < n; c++) {
+            arr[r][c] = r * n + c;
+            printf("%d ", arr[r][c]);
+        }
+    }
+
+    return 0;
+}
