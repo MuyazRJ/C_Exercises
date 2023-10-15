c03c7e58acb50d26cde377fb70f872a8364711d9
Muyaz Rahman <Muyaz154@gmail.com>
2023-10-15 22:16:15 +0100

    Improved memory usage

--- Sorts/merge.c
+++ Sorts/merge.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *mergesort();
-int *merge();
+void mergesort();
+void merge();
 
 int main(int argc, char **argv) {
     if (argc == 1) {
@@ -17,7 +17,9 @@ int main(int argc, char **argv) {
         array[i] = atoi(argv[i + 1]);
     }
 
-    array = mergesort(array, length);
+    mergesort(array, length);
+
+    printf("Size of array: %d\n", length);
 
     for (int i = 0; i < argc - 1; i++) {
         printf("%d ", array[i]);
@@ -27,8 +29,8 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-int *mergesort(int *arr, int length) {
-    if (length < 2) return arr;
+void mergesort(int *arr, int length) {
+    if (length < 2) return;
 
     const int m = length / 2;
     int *arr1 = (int *)malloc(sizeof(int) * m);
@@ -39,27 +41,24 @@ int *mergesort(int *arr, int length) {
         else arr2[i - m] = arr[i];
     }
 
-    arr1 = mergesort(arr1, m);
-    arr2 = mergesort(arr2, length - m);
-    free(arr);
+    mergesort(arr1, m);
+    mergesort(arr2, length - m);
     
-    int *merged = merge(arr1, arr2, m, (length - m));
+    merge(arr, arr1, arr2, m, (length - m));
 
     free(arr1);
     free(arr2);
-    return merged;
 }
 
-int *merge(int *arr1, int *arr2, int leftLength, int rightLength) {
-    int *merged = (int *)malloc(sizeof(int) * (leftLength + rightLength));
+void merge(int *arr, int *arr1, int *arr2, int leftLength, int rightLength) {
     int l = 0, r = 0, i = 0;
 
     while (l < leftLength && r < rightLength) {
         if (arr1[l] < arr2[r]) {
-            merged[i] = arr1[l];
+            arr[i] = arr1[l];
             l++;
         } else {
-            merged[i] = arr2[r];
+            arr[i] = arr2[r];
             r++;
         }
 
@@ -67,14 +66,12 @@ int *merge(int *arr1, int *arr2, int leftLength, int rightLength) {
     }
 
     for (l; l < leftLength; l++) {
-        merged[i] = arr1[l];
+        arr[i] = arr1[l];
         i++;
     }
 
     for (r; r < rightLength; r++) {
-        merged[i] = arr2[r];
+        arr[i] = arr2[r];
         i++;
     }
-
-    return merged;
 }
